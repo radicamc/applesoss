0c588dd6158566be9f4430e0268a1cca25b6741e
radicamc <radica@astro.umontreal.ca>
2022-07-26 11:47:24 -0400

    Update applesoss.py

--- APPLESOSS/applesoss.py
+++ APPLESOSS/applesoss.py
@@ -140,6 +140,8 @@ class EmpiricalProfile:
             filename = filepattern.format(subarray, self.oversample, self.pad)
         print('Saving to file '+(output_dir + filename))
         hdu.writeto(output_dir + filename, overwrite=True)
+        
+        return filename
 
     def validate_inputs(self):
         """Validate the input parameters.
