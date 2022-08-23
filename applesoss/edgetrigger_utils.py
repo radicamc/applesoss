73491d4522aa76dad8b3533647fd2f058abb64e0
radicamc <radica@astro.umontreal.ca>
2022-08-23 07:30:03 -0400

    edgetrigger utils docstrings

--- applesoss/edgetrigger_utils.py
+++ applesoss/edgetrigger_utils.py
@@ -13,17 +13,20 @@ from scipy.optimize import least_squares
 
 
 def zero_roll(a, shift):
-    """Like np.roll but the wrapped around part is set to zero.
-    Only works along the first axis of the array.
+    """Like np.roll but the wrapped around part is set to zero. Only works
+    along the first axis of the array.
 
-    :param a: The input array.
-    :param shift: The number of rows to shift by.
+    Parameters
+    ----------
+    a : array
+        The input array
+    shift : int
+        The number of rows to shift by.
 
-    :type a: array[any]
-    :type shift: int
-
-    :returns: result - the array with the rows shifted.
-    :rtype: array[any]
+    Returns
+    -------
+    result : np.array
+        The input array with rows shifted.
     """
 
     result = np.zeros_like(a)
@@ -38,20 +41,23 @@ def zero_roll(a, shift):
 def robust_polyfit(x, y, order, maxiter=5, nstd=3.):
     """Perform a robust polynomial fit.
 
-    :param x: x data to fit.
-    :param y: y data to fit.
-    :param order: polynomial order to use.
-    :param maxiter: number of iterations for rejecting outliers.
-    :param nstd: number of standard deviations to use when rejecting outliers.
+    Paremeters
+    ----------
+    x : array, list
+        Independent fitting variable.
+    y : array, list
+        Dependent fitting variable.
+    order : int
+        Polynomial order to fit.
+    maxiter : int
+        Number of iterations for outlier rejection.
+    nstd : float
+        Number of standard deviations to consider a value an outlier.
 
-    :type x: array[float]
-    :type y: array[float]
-    :type order: int
-    :type maxiter: int
-    :type nstd: float
-
-    :returns: param - best-fit polynomial parameters.
-    :rtype: array[float]
+    Returns
+    -------
+    res : array[float]
+        The best fitting polynomial parameters.
     """
 
     def _poly_res(p, x, y):
@@ -72,36 +78,42 @@ def robust_polyfit(x, y, order, maxiter=5, nstd=3.):
         mask = np.abs(res) <= nstd*stddev
 
     res = least_squares(_poly_res, param, loss='huber', f_scale=0.1,
-                        args=(x[mask], y[mask]))
+                        args=(x[mask], y[mask])).x
 
-    return res.x
+    return res
 
 
 def get_image_dim(image, header=None, verbose=False):
     """Determine the properties of the image array.
 
-    :param image: A 2D image of the detector.
-    :param header: The header from one of the SOSS reference files.
-    :param verbose: If set True some diagnostic plots will be made.
+    Parameters
+    ----------
+    image : array[float]
+        2D image of the detector.
+    header : astropy.io.fits.Header instance
+        The header from a SOSS reference file.
+    verbose : bool
+        If True, show diagnostic plots.
 
-    :type image: array[float]
-    :type header: astropy.io.fits.Header
-    :type verbose: bool
-
-    :returns:
-    dimx, dimy
-        The dimensions of the stack array.
-    xos, yos
-        The oversampling factors of the stack array.
-    xnative, ynative
-        The dimensions of the stack image, in native pixels.
-    padding
+    Returns
+    -------
+    dimx : int
+        X-dimension of the stack array.
+    dimy : int
+        Y-dimension of the stack array.
+    xos : int
+        Oversampling factor in the x-direction.
+    yos : int
+        Oversampling factor in the y-direction.
+    xnative : int
+        X-dimension of the stack array in native pixels.
+    ynative : int
+        Y-dimension of the stack array in native pixels.
+    padding : int
         Amount of padding around the image, in native pixels.
-    refpix_mask
-        Boolean array indicating which pixels are lightsensitive (True) and
+    refpix_mask : array[bool]
+        Boolean array indicating which pixels are light sensitive (True) and
         which are reference pixels (False).
-
-    :rtype: Tuple(int, int, int, int, int, int, int, array[bool])
     """
 
     # Dimensions of the subarray.
@@ -115,29 +127,25 @@ def get_image_dim(image, header=None, verbose=False):
         padding = 0
 
         # Assume the stack is a valid SOSS subarray.
-        # FULL: 2048x2048 or 2040x2040 (working pixels) or multiple if oversampled.
-        # SUBSTRIP96: 2048x96 or 2040x96 (working pixels) or multiple if oversampled.
-        # SUBSTRIP256: 2048x256 or 2040x252 (working pixels) or multiple if oversampled.
-
+        # FULL: 2048x2048 or 2040x2040 (working pixels) or multiple if os.
+        # SUBSTRIP96: 2048x96 or 2040x96 (working pixels) or multiple if os.
+        # SUBSTRIP256: 2048x256 or 2040x252 (working pixels) or multiple if os.
         # Check if the size of the x-axis is valid.
         if (dimx % 2048) == 0:
             xnative = 2048
             xos = int(dimx // 2048)
-
         elif (dimx % 2040) == 0:
             xnative = 2040
             xos = int(dimx // 2040)
-
         else:
             msg = ('Stack X dimension has unrecognized size of {:}. '
-                   'Accepts 2048, 2040 or multiple of.')
+                   'Accepts 2048, 2040 or multiple thereof.')
             raise ValueError(msg.format(dimx))
 
         # Check if the y-axis is consistent with the x-axis.
         if np.int(dimy/xos) in [96, 256, 252, 2040, 2048]:
             yos = np.copy(xos)
             ynative = np.int(dimy/yos)
-
         else:
             msg = ('Stack Y dimension ({:}) is inconsistent with '
                    'stack X dimension ({:}) for acceptable SOSS arrays')
